a26c072dc47ebb748df1ee03ef15ac380c1a5b39
kevin.balizet <kevin.balizet@gmail.com>
2023-01-02 14:27:28 +0100

    TP1 & ignore

--- TP/TP1/TP1/Program.cs
+++ TP/TP1/TP1/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Question 1
+
+
+class AskFindBiggerNumber
+{
+    static void Main(string[] args){
+        try{
+            Console.WriteLine("Veuillez rentrer un premier nombre:");
+                int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez rentrer un premier nombre:");
+                int n2 = int.Parse(Console.ReadLine());
+            
+            if (n1 > n2){
+                Console.WriteLine(n1 + " est le plus grand nombre");
+            }
+            else{
+                Console.WriteLine(n2 + " est le plus grand nombre");
+            }
+
+            }
+
+        catch{
+            Console.WriteLine("Vous n'avez pas saisi un nombre entier");
+        }
+        
+    }
+
+}
+
+
+//Question 2
+
+//Question 3
+
+//Question 4
+
+
